1415d859bb9f7cb4f185e9d2e1a22daaee2b50c4
Duygu Serbest <serbestduygu@yahoo.com>
2017-10-30 16:40:57 +0300

    Refactor to convert static variables to constants.

--- src/main/java/io/duygu/clustering/chart/LineChartDrawer.java
+++ src/main/java/io/duygu/clustering/chart/LineChartDrawer.java
@@ -13,24 +13,28 @@ import java.util.Map;
 public class LineChartDrawer {
 
     public static final String CHART_TITLE = "Turkish Thesis Clustering";
+
     public static final String X_AXIS_TITLE = "Cluster Count";
+
     public static final String Y_AXIS_TITLE = "Squared Error";
+
+    public static final int WIDTH = 640;
+
+    public static final int HEIGHT = 480;
+
+    public static final String CHART_JPG_FILE_NAME = "Clustering.jpeg";
 
     public static void draw(Map<Integer, Double> clusterErrorRate) throws IOException {
         DefaultCategoryDataset dataset = createDataset(clusterErrorRate);
-        JFreeChart lineChartObject = ChartFactory.createLineChart(
-                CHART_TITLE, X_AXIS_TITLE, Y_AXIS_TITLE, dataset, PlotOrientation.VERTICAL,
-                true, true, false);
-
-        int width = 640;    /* Width of the image */
-        int height = 480;   /* Height of the image */
-        File lineChart = new File("Clustering.jpeg");
-        ChartUtilities.saveChartAsJPEG(lineChart, lineChartObject, width, height);
+        JFreeChart lineChartObject = ChartFactory.createLineChart(CHART_TITLE, X_AXIS_TITLE, Y_AXIS_TITLE, dataset,
+            PlotOrientation.VERTICAL, true, true, false);
+        File lineChart = new File(CHART_JPG_FILE_NAME);
+        ChartUtilities.saveChartAsJPEG(lineChart, lineChartObject, WIDTH, HEIGHT);
     }
 
     private static DefaultCategoryDataset createDataset(Map<Integer, Double> clusterErrorRate) {
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-        clusterErrorRate.entrySet().stream().forEach(entry -> dataset.addValue(entry.getValue(), Y_AXIS_TITLE, entry.getKey()));
+        clusterErrorRate.entrySet().forEach(entry -> dataset.addValue(entry.getValue(), Y_AXIS_TITLE, entry.getKey()));
         return dataset;
     }
 }
